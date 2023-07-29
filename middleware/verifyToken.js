1212977e355b2bfbc4654d60557ef1fcc5901403
ShizuKaida <berkay.developer.albayrak>
2023-07-29 18:46:06 +0300

    Benim hatam

--- middleware/verifyToken.js
+++ middleware/verifyToken.js
@@ -5,7 +5,7 @@ module.exports = (req, res, next) => {
     req.headers["x-access-token"] || req.body.token || req.query.token;
 
   if (token) {
-    jwt.verify(token, process.env.API_SECRET_KEY, (err, decoded) => {
+    jwt.verify(token, "THIS_IS_VERY_SECRET_API_KEY", (err, decoded) => {
       if (err) {
         if (err.name === "TokenExpiredError") {
           res.status(401).json({
@@ -26,7 +26,7 @@ module.exports = (req, res, next) => {
           id: decoded.id,
           isPremiumUser: decoded.isPremiumUser,
         };
-        const newToken = jwt.sign(payload, process.env.API_SECRET_KEY, {
+        const newToken = jwt.sign(payload, "THIS_IS_VERY_SECRET_API_KEY", {
           expiresIn: 72000,
         });
         res.set("x-access-token", newToken);
