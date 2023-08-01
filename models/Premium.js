b3791cbfd50263dd2fe73dd9b4383183cb415195
ShizuKaida <berkay.developer.albayrak>
2023-08-01 17:44:50 +0300

    berkayın hatası

--- models/Premium.js
+++ models/Premium.js
@@ -1,4 +1,4 @@
-<<<<<<< HEAD
+
 const mongoose = require("mongoose");
 
 const premiumSchema = new mongoose.Schema({
@@ -13,19 +13,3 @@ const premiumSchema = new mongoose.Schema({
 
 
 module.exports = mongoose.model("Premium", premiumSchema);
-=======
-const mongoose = require("mongoose");
-
-const premiumSchema = new mongoose.Schema({
-  userId: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: "User",
-  },
-  expiresAt: {
-    type: Date,
-  },
-});
-
-
-module.exports = mongoose.model("Premium", premiumSchema);
->>>>>>> bcca3cebb0743b7bb7f6ff3ca181da7b39e84e82
